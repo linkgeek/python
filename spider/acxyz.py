69c3a00b06c9bd4c777eac5ddd016211b3deef49
linkgeek <941192051@qq.com>
2021-05-16 15:19:08 +0800

    kw

--- spider/acxyz.py
+++ spider/acxyz.py
@@ -38,8 +38,7 @@ xpath_title = './/text()'
 # 分页获取
 def get_page_data(page, data, search_str):
     page_url = url + '&page=' + str(page)
-    max_retry = 1
-    while max_retry <= 3:
+    while True:
         try:
             r = requests.get(page_url, headers=headers, timeout=15)
             r.raise_for_status()  # 如果响应状态码不是 200，就主动抛出异常
@@ -73,13 +72,13 @@ def get_page_data(page, data, search_str):
                 if bool(re.search("|".join(search_str), _title, re.I)):
                     _tip = li_each.xpath(xpath_tip)[0]
                     _href = li_each.xpath(xpath_href)[0]
-                    t = {'id': str(idx + 1), 'title': _tip + _title}
+                    t = {'title': _tip + _title}
                     full_href = base_url + _href
                     t['href'] = xlwt.Formula('HYPERLINK("{}"; "{}")'.format(full_href, full_href))
+                    # t['down'] = full_href
                     print('Page: ' + str(page) + ', No: ' + str(idx + 1) + ', Title: ' + t[
                         "title"] + ', Href: ' + full_href,
                           '\n')
-                    t['down'] = full_href
                     # print(t)
                     data.append(t)
                     time.sleep(random.randint(1, 3))
@@ -100,8 +99,6 @@ def get_page_data(page, data, search_str):
                     #         time.sleep(random.randint(1, 3))
 
             return data
-
-        max_retry += 1
 
 
 # 获取每列所占用的最大列宽
@@ -125,7 +122,7 @@ def write_xls(data, file='acxyz'):
 
     worksheet.write(0, 0, "标题")
     worksheet.write(0, 1, "链接")
-    worksheet.write(0, 2, "下载")
+    # worksheet.write(0, 2, "下载")
 
     # 列数
     # col_num = [0 for x in range(0, len(data) + 1)]
@@ -135,15 +132,19 @@ def write_xls(data, file='acxyz'):
     for i in range(len(data)):
         worksheet.write(i + 1, 0, data[i]["title"])
         worksheet.write(i + 1, 1, data[i]["href"])
-        worksheet.write(i + 1, 2, data[i]["down"])
+        # worksheet.write(i + 1, 2, data[i]["down"])
 
         # col_num[0] = len(data[i]["id"].encode('gb18030'))  # 计算每列值的大小
         # col_num[1] = len(data[i]["title"].encode('gb18030'))  # 计算每列值的大小
         # col_list.append(copy.copy(col_num))  # 记录一行每列写入的长度
 
+        # 设置行高
+        worksheet.row(i + 1).height_mismatch = True
+        worksheet.row(i + 1).height = 20 * 24  # 20为基准数，20磅
+
     # 获取每列最大宽度
     # col_max_num = get_max_col(col_list)
-    col_max_num = [100, 60, 40, 0]
+    col_max_num = [80, 60, 0]
     # print(col_max_num, '\n')
     # exit()
 
@@ -160,18 +161,18 @@ def main():
     start_time = time.perf_counter()
     data = []
 
-    search_str = "91汝工作室"
     keywords1 = ("91汝工作室", '乌克兰', '推特网红', '推特女神', '推特极品', '极品网红', '宅男福利',
-                 '私人', '女仆', '超粉嫩美鲍', '骚+浪+贱', '骚 浪 贱')
+                '私人玩物', '女仆', '超粉嫩美鲍', '骚+浪+贱', '骚 浪 贱', '91SWEATTT', 'HEGRE', '超正点')
     keywords = (
-        '有喵酱',
-        '白袜袜格罗丫', '软萌萝莉小仙', '恶犬', '香草少女', '沐沐睡不着呀',
-        '来自喵星的岁酱', '沐儿', '闵儿', "初音", "不见星空", '原歆公主', '涂鸦少女', '你们的小秋秋',
-        '完具', '娜美妖姬', '依灵儿', '比卡丘', '亲亲我吖', '我刚成年', '软糖呀', '有喵酱',
-        '工口糯米姬', '范冰冰', '橘猫', '九尾狐狸', '奈音', '小清殿下', "喵喵儿", '樱井奈奈', '怪污可优',
-        '希希酱', '夜夜主教', '赛高酱', '貂蝉', '王星雅', '王瑞儿', '姚安琪', 'LEXISCANDYSHOP')
-    file_name = "111"
-    for i in range(1, 6):
+        '有喵酱', '小鸟酱', '苏苏', '工口糯米姬', '押尾猫', "初音", "不见星空",
+        '白袜袜格罗丫', '软萌萝莉小仙', '绯红小猫', '恶犬', '香草少女', '沐沐睡不着呀', '呆萌橘子酱', '希希酱',
+        '来自喵星的岁酱', '沐儿', '露西宝宝', '闵儿', '原歆公主', '涂鸦少女', '小秋秋',
+        '完具', '娜美妖姬', '依灵儿', '比卡丘', '亲亲我吖', '我刚成年', '软糖呀', '有喵酱', '桃桃酱',
+        '范冰冰', '橘猫', '九尾狐狸', '奈音', '小清殿下', "喵喵儿", '樱井奈奈', '怪污可优', '悠宝', '械师',
+        '夜夜主教', '赛高酱', '貂蝉', '王星雅', '王瑞儿', '姚安琪', 'LEXISCANDYSHOP', '浪味仙儿',
+        '三寸', '萌兰酱', '发条', '习呆呆')
+    file_name = "keywords2_0516"
+    for i in range(1, 50):
         data = get_page_data(i, data, keywords)
         print('page' + str(i) + ' done!')
         time.sleep(random.randint(2, 4))
